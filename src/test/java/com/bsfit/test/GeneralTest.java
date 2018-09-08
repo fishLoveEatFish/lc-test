45751307bf9b9da89a27a0bbcbe2d8a68a8ac518
lc <lcyiyouxing@163.com>
2018-09-08 15:11:36 +0800

    three test

---
+++ src/test/java/com/bsfit/test/GeneralTest.java
@@ -0,0 +1,15 @@
+package com.bsfit.test;
+
+import org.junit.Test;
+
+/**
+ * 普通测试类
+ * @author lc
+ */
+public class GeneralTest {
+    @Test
+    public void test(){
+        System.out.println("11");
+        System.out.println("我要测试git哦");
+    }
+}
